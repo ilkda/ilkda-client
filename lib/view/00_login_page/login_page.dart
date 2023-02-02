b290dfd2dfe4d3e538875d6ba9f6fa462740876c
HyeonJunSong <music9801@naver.com>
2023-02-02 16:22:52 +0900

    refactor: Modify kakao login function
    Refactored kakao login funcion. Moved the function to Individual file. p.s. added initial code to check token from local secure storage for test.

--- lib/view/00_login_page/login_page.dart
+++ lib/view/00_login_page/login_page.dart
@@ -6,8 +6,26 @@ import 'package:ilkda_client/view/00_login_page/test_page.dart';
 import 'package:ilkda_client/view_model/user_controller.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-class LogInPage extends StatelessWidget {
+class LogInPage extends StatefulWidget {
   const LogInPage({Key? key}) : super(key: key);
+
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Get.find<UserController>().ifKakaoTokenInLocalStorage().then((value) => {
+      if(value){
+        Get.to(() => TestPage(text: Get.find<UserController>().kakaoToken.value))
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,43 +68,12 @@ class LogInPage extends StatelessWidget {
 
   _kakaoButton(BuildContext context) => ElevatedButton(
     onPressed: () async {
-      // 카카오 로그인 구현 예제
-
-      // 카카오톡 실행 가능 여부 확인
-      // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-
-      OAuthToken token;
-      if (await isKakaoTalkInstalled()) {
-        try {
-          token = await UserApi.instance.loginWithKakaoTalk();
-          print(token.accessToken);
-          Get.to(() => TestPage(text: token.accessToken,));
-          print('카카오톡으로 로그인 성공');
-        } catch (error) {
-          print('카카오톡으로 로그인 실패 $error');
-
-          // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-          // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-          if (error is PlatformException && error.code == 'CANCELED') {
-            return;
-          }
-          // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-          try {
-            token = await UserApi.instance.loginWithKakaoAccount();
-            Get.to(() => TestPage(text: token.accessToken,));
-            print('카카오계정으로 로그인 성공');
-          } catch (error) {
-            print('카카오계정으로 로그인 실패 $error');
-          }
-        }
-      } else {
-        try {
-          token = await UserApi.instance.loginWithKakaoAccount();
-          Get.to(() => TestPage(text: token.accessToken,));
-          print('카카오계정으로 로그인 성공');
-        } catch (error) {
-          print('카카오계정으로 로그인 실패 $error');
-        }
+      if( await Get.find<UserController>().tryKakaoLogin()){
+        print("login success!");
+        Get.to(() => TestPage(text: Get.find<UserController>().kakaoToken.value));
+      }
+      else{
+        print("login failed!");
       }
     },
 
@@ -122,5 +109,4 @@ class LogInPage extends StatelessWidget {
       ],
     ),
   );
-
 }
--- lib/view_model/user_controller.dart
+++ lib/view_model/user_controller.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
-import 'package:ilkda_client/model/servicies/kakao_oauth_token_from_local_storage.dart';
+import 'package:ilkda_client/model/servicies/kakao_login/kakao_oauth_login.dart';
+import 'package:ilkda_client/model/servicies/kakao_login/kakao_oauth_token_from_local_storage.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class UserController extends GetxController{
 
-  //Kakao OAuth Token
+  //Kakao LogIn
   RxString kakaoToken = "".obs;
 
   Future<bool> ifKakaoTokenInLocalStorage() async {
@@ -17,4 +19,17 @@ class UserController extends GetxController{
       return true;
     }
   }
+
+  Future<bool> tryKakaoLogin() async {
+    OAuthToken? token = await kakaoLogin();
+    if(token == null){
+      return false;
+    }
+    else{
+      writeKakaoOauthTokenToLocalStorage(token.accessToken);
+      kakaoToken(token.accessToken);
+      return true;
+    }
+  }
+
 }
