fa33ebd0e376c903c0da78f44b88c5a7a2773327
HyeonJunSong <music9801@naver.com>
2023-02-13 20:55:57 +0900

    feat: implement home page
    implemented home page and connected it with view controller.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ilkda_client/view/00_login_page/login_page.dart';
+import 'package:ilkda_client/view/10_home_page/home_page.dart';
+import 'package:ilkda_client/view_model/home_page_viewcontroller.dart';
 import 'package:ilkda_client/view_model/user_controller.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
@@ -21,10 +23,9 @@ class IlkdaClient extends StatelessWidget {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: '읽다',
-          initialRoute: "/LogIn",
+          initialRoute: "/Home",
           initialBinding: BindingsBuilder(() {}),
           getPages: [
-
             GetPage(
               name: "/LogIn",
               page: () => const LogInPage(),
@@ -32,7 +33,13 @@ class IlkdaClient extends StatelessWidget {
                 Get.put(UserController());
               }),
             ),
-
+            GetPage(
+              name: "/Home",
+              page: () => const HomePage(),
+              binding: BindingsBuilder(() {
+                Get.put(HomePageViewController());
+              }),
+            ),
           ],
 
           theme: ThemeData(
