d46f0218294565900045e5e37be131ecb660255f
HyeonJunSong <music9801@naver.com>
2023-02-27 13:54:31 +0900

    feat: featured search page view controller & model
    featured search page view controller, book model api part,
    and connected view controller to view

--- lib/model/book.dart
+++ lib/model/book.dart
@@ -1,8 +1,18 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:ilkda_client/constants/key.dart';
+
+import 'package:ilkda_client/servicies/api.dart';
+import 'package:ilkda_client/view_model/user_controller.dart';
 
 class Book{
   int id;
   String title;
   String author;
+  String publisher;
+  String publishedDate;
   int page;
   String cover;
 
@@ -10,7 +20,102 @@ class Book{
     required this.id,
     required this.title,
     required this.author,
+    required this.publisher,
+    required this.publishedDate,
     required this.page,
     required this.cover,
   });
+
+  //////////////////////////////////////////////////////////////////////////////Search Book List
+  static List<Book> SearchBooksJson(dynamic json){
+    List<Book> newBooks = <Book>[];
+    List<dynamic>.from(Map<String, dynamic>.from(json)["response"]).forEach((element) {
+      Map<String, dynamic> e = Map<String, dynamic>.from(element);
+      newBooks.add(
+        Book(
+          id: e["id"],
+          title: e["title"],
+          author: e["author"],
+          publisher: "publisher",
+          publishedDate: "publishedDate",
+          page: e["page"],
+          cover: e["cover"]
+        )
+      );
+    });
+
+    return newBooks;
+  }
+
+  static Future<List<Book>> GETSearchBookList({
+    String title = "",
+    int page = 0,
+    int size = 10
+  }) async {
+    ApiResponse apiResponse = ApiResponse();
+
+    try {
+      final response = await http.get(
+        Uri.parse("http://" + baseUrl + '/' + URLbookListSearch + '?' + "title=${title}&page=${page}&size=${size}"),
+        headers: {
+          "Authorization" : "Bearer " + Get.find<UserController>().accessToken.value
+        }
+      );
+
+      print(response.body);
+
+      switch (response.statusCode) {
+        case 200:
+          return Book.SearchBooksJson(json.decode(utf8.decode(response.bodyBytes)));
+        case 401:
+          apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
+          break;
+        default:
+          apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
+          break;
+      }
+    } on SocketException {
+      apiResponse.apiError = ApiError(error: "Server error. Please retry");
+    }
+
+    return <Book>[];
+  }
+
+  //////////////////////////////////////////////////////////////////////////////register Book on Read List
+  static Future<String> RegisterBookJson(dynamic json){
+    return Map<String, dynamic>.from(json)["response"];
+  }
+
+  static Future<String> POSTReisterBook({required int id}) async {
+    ApiResponse apiResponse = ApiResponse();
+
+    try {
+      final response = await http.post(
+        Uri.parse("http://" + baseUrl + '/' + URLregisterBook),
+        headers: {
+          "Authorization" : "Bearer " + Get.find<UserController>().accessToken.value,
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode({
+          "BookId": id
+        })
+      );
+
+      print(response.body);
+
+      switch (response.statusCode) {
+        case 200:
+          return Book.RegisterBookJson(json.decode(utf8.decode(response.bodyBytes)));
+        default:
+          apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
+          return response.statusCode.toString();
+      }
+    } on SocketException {
+      apiResponse.apiError = ApiError(error: "Server error. Please retry");
+    }
+
+    return "failed!";
+  }
+
+
 }
--- lib/view_model/home_page_viewcontroller.dart
+++ lib/view_model/home_page_viewcontroller.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:get/get.dart';
 import 'package:ilkda_client/model/aladin_book.dart';
+import 'package:ilkda_client/model/book.dart';
 
 class HomePageViewController extends GetxController{
   @override
@@ -9,6 +10,7 @@ class HomePageViewController extends GetxController{
     searchAladinBooks();
   }
 
+  //////////////////////////////////////////////////////////////////////////////home page
   //Aladin Recommended BookLists
   RxList<AladinBook> aladinBooks = <AladinBook>[].obs;
   void updateAladinBooks(List<AladinBook> newAladinBooks) {
@@ -22,4 +24,31 @@ class HomePageViewController extends GetxController{
         updateAladinBooks(value);
       });
   }
+
+
+  //////////////////////////////////////////////////////////////////////////////search page
+  TextEditingController searchController = TextEditingController();
+
+  RxList<Book> searchBooksList = <Book>[].obs;
+
+  void updateSearchBooksList(List<Book> newBooks){
+    searchBooksList(newBooks);
+  }
+
+  void searchBookList() async {
+    updateSearchBooksList(
+      await Book
+      .GETSearchBookList(
+        title: searchController.text,
+      )
+    );
+  }
+
+  void registerBook(int bookID) async {
+    await Book
+      .POSTReisterBook(
+        id: bookID,
+      );
+  }
+
 }
