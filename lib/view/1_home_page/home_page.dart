dc1481022456d8e1c28166d75bbd3ba92547065e
HyeonJunSong <music9801@naver.com>
2023-03-12 19:41:49 +0900

    feat: home page initstate
    added home page init state code
    get aladin books, record lists

--- lib/view/1_home_page/home_page.dart
+++ lib/view/1_home_page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:ilkda_client/view_model/home_page_viewcontroller.dart';
 
 import 'widget_app_bar.dart';
 import 'widget_my_book_list.dart';
@@ -8,8 +9,21 @@ import 'widget_new_book_list.dart';
 import 'widget_navigation_bar.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Get.find<HomePageViewController>().searchAladinBooks();
+    Get.find<HomePageViewController>().searchCurrentRecordList();
+  }
 
   @override
   Widget build(BuildContext context) {
