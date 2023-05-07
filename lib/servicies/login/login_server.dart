1e16375ab3ced3e14ca6883f91bfff3805c0e479
HyeonJunSong <music9801@naver.com>
2023-05-07 17:45:28 +0900

    comment: logging just for development

--- lib/servicies/login/login_server.dart
+++ lib/servicies/login/login_server.dart
@@ -10,9 +10,9 @@ import 'package:ilkda_client/servicies/login/get_refresh_token_from_local_storag
 ////try to login with server
 //if success, return accessToken, and store refreshToken in local storage
 Future<String> POSTtryServerSignUP(String kakaoAccessToken) async {
-  ApiResponse apiResponse = ApiResponse();
-
   try {
+    print(kakaoAccessToken);
+
     final response = await http.post(
       Uri.parse('http://${baseUrl}/${URLsignUp}'),
       headers: {
@@ -23,21 +23,22 @@ Future<String> POSTtryServerSignUP(String kakaoAccessToken) async {
       })
     );
 
+    print(response.body);
+
     switch (response.statusCode) {
       case 200:
         Map<String, dynamic> responseObj = Map<String, dynamic>.from(jsonDecode(utf8.decode(response.bodyBytes)))["response"];
         writeRefreshOauthTokenToLocalStorage(responseObj["refreshToken"]);
         // print("access Token : ${responseObj["accessToken"]}");
         return responseObj["accessToken"];
-      case 401:
-        apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
-        break;
       default:
-        apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
+        print("Error at POSTtryServerSignin");
+        print("[${response.statusCode}]");
+        print(response.body);
         break;
     }
   } on SocketException {
-    apiResponse.apiError = ApiError(error: "Server error. Please retry");
+    print("No Internet connection");
   }
 
   return "";
@@ -46,8 +47,6 @@ Future<String> POSTtryServerSignUP(String kakaoAccessToken) async {
 ////try to login with server
 //if success, return accessToken, and store refreshToken in local storage
 Future<String> POSTtryServerSignin(String ilkdaRefreshToken) async {
-  ApiResponse apiResponse = ApiResponse();
-
   try {
     final response = await http.post(
         Uri.parse('http://${baseUrl}/${URLsignIn}'),
@@ -67,15 +66,15 @@ Future<String> POSTtryServerSignin(String ilkdaRefreshToken) async {
         writeRefreshOauthTokenToLocalStorage(responseObj["refreshToken"]);
         // print("access Token : ${responseObj["accessToken"]}");
         return responseObj["accessToken"];
-      case 401:
-        apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
-        break;
+
       default:
-        apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
+        print("Error at POSTtryServerSignin");
+        print("[${response.statusCode}]");
+        print(response.body);
         break;
     }
   } on SocketException {
-    apiResponse.apiError = ApiError(error: "Server error. Please retry");
+    print("No Internet connection");
   }
 
   return "";
