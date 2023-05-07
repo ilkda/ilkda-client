1216a75459b8b3cc2f4713d696291e5448009f18
HyeonJunSong <music9801@naver.com>
2023-05-07 17:49:50 +0900

    chore: extra work for home page design reformation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:ilkda_client/view/00_loading_page/loading_page.dart';
+import 'package:ilkda_client/view/01_login_page/login_page.dart';
 import 'package:ilkda_client/view/12_read_book_page/read_book_page.dart';
 import 'package:ilkda_client/view/13_book_report_page/book_report_page.dart';
 import 'package:ilkda_client/view/20_book_shelf_page/book_shelf_page.dart';
@@ -9,7 +11,6 @@ import 'package:ilkda_client/view_model/book_shelf_page_viewcontroller.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 import 'constants/key.dart';
-import 'view/0_login_page/login_page.dart';
 import 'view/10_home_page/home_page.dart';
 import 'view_model/home_page_viewcontroller.dart';
 import 'view_model/user_controller.dart';
@@ -37,7 +38,14 @@ class IlkdaClient extends StatelessWidget {
           getPages: [
             GetPage(
               name: "/",
-              page: () => const LogInPage(),
+              page: () => LoadingPage(),
+              binding: BindingsBuilder(() {
+                Get.put(UserController());
+              }),
+            ),
+            GetPage(
+              name: "/LogIn",
+              page: () => LogInPage(),
               binding: BindingsBuilder(() {
                 Get.put(UserController());
               }),
@@ -49,7 +57,6 @@ class IlkdaClient extends StatelessWidget {
               binding: BindingsBuilder(() {
                 Get.put(HomePageViewController());
               }),
-              transition: Transition.leftToRight,
             ),
             GetPage(
               name: "/Home/Search",
@@ -77,18 +84,21 @@ class IlkdaClient extends StatelessWidget {
                 Get.put(HomePageViewController());
                 Get.put(BookShelfPageViewController());
               }),
-              transition: Transition.rightToLeft,
             ),
           ],
 
           theme: ThemeData(
-            fontFamily: "RIDIBatang",
+            fontFamily: "NotoSansKR",
 
-
-            primaryColorDark: Colors.black,
-            primaryColorLight: Colors.white,
-            primaryColor: Color(0xFFFF918D),
-            backgroundColor: Colors.white,
+            colorScheme: const ColorScheme.light(
+              primary: Color(0xFFFF7A8A),
+              outline: Color(0xFF000000),
+              onSurface: Color(0xFF585858),
+              surface: Color(0xFF9E9E9E),
+              onBackground: Color(0xFFCFCFCF),
+              onPrimary: Color(0xFFF2F2F2),
+              background: Color(0xFFFFFFFF),
+            ),
           ),
         );
       }
