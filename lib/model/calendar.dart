a48c8603a6f46fee01b8916faffc94e5e070e576
HyeonJunSong <music9801@naver.com>
2023-05-09 17:11:35 +0900

    feat: calendar
    get data from server and parse
    -> get Lists of calendar element(level and read count)
    level is -1 if it is not valid day, and 0 when read count is 0.
    calendar view code is quite dirty. It should be optimized later

--- lib/model/calendar.dart
+++ lib/model/calendar.dart
@@ -8,7 +8,7 @@ import 'package:ilkda_client/view_model/user_controller.dart';
 
 class Calendar{
   CalendarInfo calendarInfo;
-  List<List<CalendarElement>> calendarElements;
+  List<CalendarElement> calendarElements;
 
   Calendar({
     required this.calendarInfo,
@@ -20,70 +20,60 @@ class Calendar{
     this.calendarElements = const []
   });
 
+  bool isNull(){
+    return calendarInfo.isNull();
+  }
+
   //////////////////////////////////////////////////////////////////////////////[GET] User Calendar from server
-  static Calendar calendarFromJson(CalendarInfo calendarInfo, int year, int month, dynamic json){
+  static Calendar calendarFromJson(CalendarInfo calendarInfo, dynamic json){
+
     Calendar newCalendar = Calendar(
       calendarInfo: calendarInfo,
       calendarElements: []
     );
 
-    List<dynamic> records = List<dynamic>.from(Map<String, dynamic>.from(json)["response"]);
-
-    DateTime thisMonth = DateTime.parse("$year-$month-01");
-    int thisMonthDays = DateTime(thisMonth.year, thisMonth.month + 1, 0).day;
-    ////Calendar Init
-    newCalendar.calendarElements.add([]);
-    for(int i = 0; i < (thisMonth.weekday % 7); i++){
-      newCalendar.calendarElements[0].add(CalendarElement(
-        date: "${year}-${month}-${i + 1}",
-        level: 0,
-        readPageCount: 0
-      ));
-    }
-    for(int i = 0; i < thisMonthDays; i++){
-      if(i % 7 == 0) newCalendar.calendarElements.add([]);
-      newCalendar.calendarElements.last.add(CalendarElement.nullInit());
+    for(int i = 0; i < calendarInfo.thisMonth.day; i++){
+      newCalendar.calendarElements.add(CalendarElement.nullInit(level: 0));
     }
 
     ////Calendar Data
     List<dynamic> response = List<dynamic>.from(Map<String, dynamic>.from(json)["response"]);
-    response.forEach((element) {
+    for (var element in response) {
       Map<String, dynamic> e = Map<String, dynamic>.from(element);
       DateTime date = DateTime.parse(e["date"]);
-      newCalendar.calendarElements[((date.day + thisMonth.weekday & 7) / 7) as int][date.weekday % 7] = CalendarElement(
-        date: e["date"],
-        level: 0,
-        readPageCount: e["readPageCount"]
-      );
-    });
+      newCalendar.calendarElements[date.day].readPageCount = e["readPageCount"];
+      newCalendar.calendarElements[date.day].level = CalendarElement.calculateLevel(e["readPageCount"], calendarInfo.yearMinReadPageCount, calendarInfo.yearMaxReadPageCount);
+    }
 
-    return Calendar.nullInit(calendarInfo: CalendarInfo.nullInit());
+    return newCalendar;
   }
 
   static Future<Calendar> GETUserCalendar({
-    required int year,
-    required int month,
+    required DateTime thisMonth,
   }) async {
 
-    CalendarInfo calendarInfo = await CalendarInfo.GETUserCalendarInfo(year: year, month: month);
-    if(calendarInfo.isNull()) return Calendar.nullInit(calendarInfo: CalendarInfo.nullInit());
+    CalendarInfo calendarInfo = await CalendarInfo.GETUserCalendarInfo(thisMonth: thisMonth);
+    if(calendarInfo.isNull()) return Calendar.nullInit(
+      calendarInfo: CalendarInfo.nullInit(
+        thisMonth: calendarInfo.thisMonth
+      )
+    );
 
     ApiResponse apiResponse = ApiResponse();
 
     try {
       final response = await http.get(
-          Uri.parse("http://$baseUrl/$URLRecord/$URLCalendarData?year=${year}&month=${month}"),
+          Uri.parse("http://$baseUrl/$URLRecord/$URLCalendarData?year=${thisMonth.year}&month=${thisMonth.month}"),
           headers: {
             "Authorization" : "Bearer " + Get.find<UserController>().accessToken.value
           }
       );
 
-      print(utf8.decode(response.bodyBytes));
+      // print(utf8.decode(response.bodyBytes));
 
       switch (response.statusCode) {
         case 200:
-          return Calendar.calendarFromJson(calendarInfo, year, month, json.decode(utf8.decode(response.bodyBytes)));
-          // return Calendar.CalendarFromJson(json.decode(utf8.decode(response.bodyBytes)));
+          return Calendar.calendarFromJson(calendarInfo, json.decode(utf8.decode(response.bodyBytes)));
         case 401:
           apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
           break;
@@ -95,55 +85,76 @@ class Calendar{
       apiResponse.apiError = ApiError(error: "Server error. Please retry");
     }
 
-    return Calendar.nullInit(calendarInfo: CalendarInfo.nullInit());
+    return Calendar.nullInit(
+      calendarInfo: CalendarInfo.nullInit(
+        thisMonth: calendarInfo.thisMonth,
+      )
+    );
   }
 }
 
 class CalendarElement {
-  String date;
   int level;
   int readPageCount;
 
   CalendarElement({
-    required this.date,
     required this.level,
     required this.readPageCount
   });
 
   CalendarElement.nullInit({
-    this.date = "",
-    this.level = 0,
+    this.level = -1,
     this.readPageCount = 0
   });
+
+  static int calculateLevel(int curCnt, int min, int max){
+    if(curCnt == 0)
+      return curCnt;
+    if(min <= curCnt && curCnt < (max - min) / 2)
+      return 1;
+    if((max - min) / 2 <= curCnt && curCnt <= max)
+      return 2;
+    return -1;
+  }
 }
 
 class CalendarInfo{
+  DateTime thisMonth;
   int yearMaxReadPageCount;
   int yearMinReadPageCount;
   int monthReadDateCount;
+  int firstWeekdayOfMonth;
 
   CalendarInfo({
+    required this.thisMonth,
     required this.yearMaxReadPageCount,
     required this.yearMinReadPageCount,
-    required this.monthReadDateCount
-  });
+    required this.monthReadDateCount,
+    this.firstWeekdayOfMonth = -1,
+  }){
+    firstWeekdayOfMonth = DateTime(thisMonth.year, thisMonth.month, 1).weekday;
+  }
 
   //////////////////////////////////////////////////////////////////////////////[NULL] Calendar Info
   CalendarInfo.nullInit({
+    required this.thisMonth,
     this.yearMaxReadPageCount = -1,
     this.yearMinReadPageCount = -1,
-    this.monthReadDateCount = -1
+    this.monthReadDateCount = -1,
+    this.firstWeekdayOfMonth = -1,
   });
   bool isNull(){
     return yearMaxReadPageCount == -1;
   }
 
-  //////////////////////////////////////////////////////////////////////////////[GET]
-
   //////////////////////////////////////////////////////////////////////////////[GET] Calendar Info From User
-  static CalendarInfo calendarInfoFromJson(dynamic json){
-    Map<String, dynamic> response = Map<String, dynamic>.from(json)["response"];
+  static CalendarInfo calendarInfoFromJson(
+    dynamic json,{
+    required DateTime thisMonth,
+  }){
+    Map<String, dynamic> response = Map<String, dynamic>.from(Map<String, dynamic>.from(json)["response"]);
     return CalendarInfo(
+      thisMonth: thisMonth,
       yearMaxReadPageCount: response["yearMaxReadPageCount"],
       yearMinReadPageCount: response["yearMinReadPageCount"],
       monthReadDateCount: response["monthReadDateCount"]
@@ -151,24 +162,26 @@ class CalendarInfo{
   }
 
   static Future<CalendarInfo> GETUserCalendarInfo({
-    required int year,
-    required int month,
+    required DateTime thisMonth,
   }) async {
     ApiResponse apiResponse = ApiResponse();
 
     try {
       final response = await http.get(
-          Uri.parse("http://$baseUrl/$URLRecord/$URLCalendarInfo?year=${year}&month=${month}"),
+          Uri.parse("http://$baseUrl/$URLRecord/$URLCalendarInfo?year=${thisMonth.year}&month=${thisMonth.month}"),
           headers: {
             "Authorization" : "Bearer " + Get.find<UserController>().accessToken.value
           }
       );
 
-      print(utf8.decode(response.bodyBytes));
+      // print(utf8.decode(response.bodyBytes));
 
       switch (response.statusCode) {
         case 200:
-          return CalendarInfo.calendarInfoFromJson(json.decode(utf8.decode(response.bodyBytes)));
+          return CalendarInfo.calendarInfoFromJson(
+            thisMonth: thisMonth,
+            json.decode(utf8.decode(response.bodyBytes)),
+          );
       // return Calendar.CalendarFromJson(json.decode(utf8.decode(response.bodyBytes)));
         case 401:
           apiResponse.apiError = ApiError.fromJson(json.decode(response.body));
@@ -181,6 +194,8 @@ class CalendarInfo{
       apiResponse.apiError = ApiError(error: "Server error. Please retry");
     }
 
-    return CalendarInfo.nullInit();
+    return CalendarInfo.nullInit(
+      thisMonth: thisMonth
+    );
   }
 }
--- lib/view_model/book_shelf_page_viewcontroller.dart
+++ lib/view_model/book_shelf_page_viewcontroller.dart
@@ -7,45 +7,24 @@ class BookShelfPageViewController extends GetxController {
   Rx<int> year = 2023.obs;
   Rx<int> month = 3.obs;
 
-  Rx<Calendar> calendar = Calendar.nullInit(calendarInfo: CalendarInfo.nullInit()).obs;
+  Rx<Calendar> calendar = Calendar.nullInit(
+      calendarInfo: CalendarInfo.nullInit(
+        thisMonth: DateTime(
+          DateTime.now().year,
+          DateTime.now().month + 1,
+          0
+        ),
+      )
+  ).obs;
 
-  void updateCalendar() async {
-    calendar(await Calendar.GETUserCalendar(year: year.value, month: month.value));
+  void updateCalendar(DateTime targetDateTime) async {
+    calendar(await Calendar.GETUserCalendar(thisMonth: targetDateTime));
+    calendar.refresh();
   }
 
-  // RxList<List<int>> calendar = [
-  //   [ 1,  0,  2,  1,  1,  2,  0],
-  //   [ 1,  0,  2,  1,  2,  0,  1],
-  //   [ 1,  1,  1,  2,  0,  2,  0],
-  //   [ 0,  2,  1,  0,  2,  0,  0],
-  // ].obs;
-
-  // RxList<List<int>> calendar = [
-  //   [-1, -1, -1, -1, -1,  2,  0],
-  //   [ 1,  0,  2,  1,  2,  0,  1],
-  //   [ 1,  1,  1,  2,  0,  2,  0],
-  //   [ 1,  1,  1,  2,  0,  2,  0],
-  //   [ 0,  2,  1,  0, -1, -1, -1],
-  // ].obs;\\
-
-  // RxList<List<int>> calendar = [
-  //   [-1, -1, -1, -1, -1,  0,  0],
-  //   [ 0,  0,  0,  0,  0,  0,  0],
-  //   [ 0,  0,  0,  0,  0,  0,  0],
-  //   [ 0,  0,  0,  0,  0,  0,  0],
-  //   [ 0,  0,  0,  0, -1, -1, -1],
-  // ].obs;
-
-
-  // RxList<List<int>> calendar = [
-  //   [-1, -1, -1, -1, -1,  2,  0],
-  //   [ 1,  0,  2,  1,  2,  0,  1],
-  //   [ 1,  1,  1,  2,  0,  2,  0],
-  //   [ 1,  1,  1,  2,  0,  2,  0],
-  //   [ 1,  0,  2,  1,  2,  0,  1],
-  //   [ 0, -1, -1, -1 , -1, -1, -1],
-  // ].obs;
-
+  bool isCalendarNull(){
+    return calendar.value.isNull();
+  }
 
   //////////////////////////////////////////////////////////////////////////////My Records
 
