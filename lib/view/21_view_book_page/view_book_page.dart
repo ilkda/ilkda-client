4fb96efdd7b55aee7dd54fc66ecad05f3b6abc0d
HyeonJunSong <music9801@naver.com>
2023-05-24 19:41:36 +0900

    feat: book page

--- lib/view/21_view_book_page/view_book_page.dart
+++ lib/view/21_view_book_page/view_book_page.dart
@@ -1,29 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:ilkda_client/model/record.dart';
+import 'package:ilkda_client/view/21_view_book_page/widget_navigation_bar.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 import 'widget_app_bar.dart';
 import 'widget_book.dart';
-import 'widget_read_control.dart';
 
-class ViewBookPage extends StatelessWidget {
-  const ViewBookPage({Key? key}) : super(key: key);
+class ViewBookPage extends StatefulWidget {
+  final Record record;
+
+  const ViewBookPage({
+    Key? key,
+    required this.record,
+  }) : super(key: key);
+
+  @override
+  State<ViewBookPage> createState() => _ViewBookPageState();
+}
+
+class _ViewBookPageState extends State<ViewBookPage> {
+  Color backgroundColor = Colors.white;
+  @override
+  void initState() {
+    super.initState();
+
+    getImagePalette(NetworkImage(widget.record.book.cover)).then((value) {
+      setState(() {
+        backgroundColor = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: false,
+      backgroundColor: backgroundColor,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(height: 47.h,),
-          appBarViewBookPage(),
-          SizedBox(height: 17.h,),
-          bookSection(),
-          SizedBox(height: 34.h,),
-          readControlSection(context),
+          SizedBox(height: 69.h,),
+          appBarViewBookPage(context),
+          SizedBox(height: 33.41.h,),
+          bookSection(context, widget.record),
+          SizedBox(height: 64.h,),
+          navigationBarViewBookPage(),
         ],
       ),
-    ));
+    );
+  }
+
+  Future<Color> getImagePalette (ImageProvider imageProvider) async {
+    final PaletteGenerator paletteGenerator = await PaletteGenerator
+      .fromImageProvider(imageProvider);
+    return paletteGenerator.lightMutedColor!.color;
   }
 }
--- lib/view/21_view_book_page/widget_book.dart
+++ lib/view/21_view_book_page/widget_book.dart
@@ -2,49 +2,133 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ilkda_client/model/book.dart';
+import 'package:ilkda_client/model/record.dart';
 import 'package:ilkda_client/view_model/home_page_viewcontroller.dart';
 
-Widget bookSection() {
+Widget bookSection(BuildContext context, Record record) {
   return Container(
-    width: 275.w,
-    height: 479.h,
+    width: 258.w,
+    height: 553.h,
     child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        _bookImage(),
-        _bookTitle(),
-        SizedBox(height: 15.h),
-        _bookAuthor(),
+        _bookImage(record.book.cover),
+        _bookInfo(context, record),
+        SizedBox(height: 15.h,),
+        _bookRecordButton(context, record.review),
       ],
     ),
   );
 }
 
 ////////////////////////////////////////////////////////////////////////////////elements
-Widget _bookImage() => Container(
-  height: 433.76.h,
-  child: Image.network(
-    Get.find<HomePageViewController>().currentBookRecord.value.book.cover,
-  ),
-);
 
-Widget _bookTitle() => Container(
-  width: 193.w,
-  child: Text(
-    Get.find<HomePageViewController>().currentBookRecord.value.book.title,
-    style: TextStyle(
-      fontSize: 14.sp,
-      fontWeight: FontWeight.w400,
-      overflow: TextOverflow.ellipsis,
+////Image
+Widget _bookImage(String cover) => Container(
+  height: 400.h,
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(6.r),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.1),
+        spreadRadius: 0,
+        blurRadius: 10,
+        offset: Offset(0, 5), // changes position of shadow
+      ),
+    ],
+  ),
+  child: ClipRRect(
+    borderRadius: BorderRadius.circular(6.r),
+    child: Image.network(
+      cover,
+      fit: BoxFit.cover,
     ),
   ),
 );
 
-Widget _bookAuthor() => Container(
+////Info
+Widget _bookInfo(BuildContext context, Record record){
+  return Column(
+    children: [
+      _bookTitle(context, record.book.title),
+      SizedBox(width: 8.h,),
+      _bookAuthor(context, record.book.author),
+      _bookPage(context, record.book.page),
+    ],
+  );
+}
+
+Widget _bookTitle(BuildContext context, String title) => Container(
+  width: 193.w,
   child: Text(
-    Get.find<HomePageViewController>().currentBookRecord.value.book.author,
+    title,
+    style: TextStyle(
+      fontSize: 14.sp,
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.ellipsis,
+      color: context.theme.colorScheme.outline,
+    ),
+  ),
+);
+
+Widget _bookAuthor(BuildContext context, String author) => Container(
+  child: Text(
+    author,
+    style: TextStyle(
+      fontSize: 10.sp,
+      fontWeight: FontWeight.w300,
+      overflow: TextOverflow.ellipsis,
+      color: context.theme.colorScheme.outline,
+    ),
+  ),
+);
+
+Widget _bookPage(BuildContext context, int page) => Container(
+  child: Text(
+    "$page쪽",
     style: TextStyle(
       fontSize: 12.sp,
-      fontWeight: FontWeight.w300,
+      fontWeight: FontWeight.w400,
+      color: context.theme.colorScheme.outline,
+    ),
+  ),
+);
+
+////Record
+Widget _bookRecordButton(BuildContext context, String review) => GestureDetector(
+  onTap : (){
+    Get.toNamed(
+      "/BookShelf/ViewBookReview",
+      arguments: {"review": review},
+    );
+  },
+  child: Container(
+    width: 107.w,
+    height: 40.h,
+    decoration: BoxDecoration(
+      color: context.theme.colorScheme.background,
+      borderRadius: BorderRadius.circular(11.r),
+      boxShadow: [
+        BoxShadow(
+          color: context.theme.colorScheme.outline.withOpacity(0.5),
+          blurRadius: 5,
+          spreadRadius: 1,
+          offset: Offset(2, 2),
+        ),
+      ],
+    ),
+    alignment: Alignment.center,
+    padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset("assets/images/pencil.png", width: 14.w, height: 14.h,),
+        Text("감상 기록", style: TextStyle(
+          fontSize: 14.sp,
+          fontWeight: FontWeight.w400,
+          height: 1.0,
+        ),),
+      ],
     ),
   ),
 );
